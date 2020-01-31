0e3bfa0b1c4f10131531865f5a66bed9d6ce55e5
Bonnix <bonnbonito@gmail.com>
2020-01-31 11:27:05 +0800

    SSR Fontawesome 2

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,12 +10,14 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import Footer from "./Footer"
-import { library } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { faTwitter, faFacebookF, faGooglePlusG } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./layout.css"
 
+config.autoAddCss = false;
 library.add(faEnvelope, faPhoneAlt, faTwitter, faFacebookF,faGooglePlusG)
 
 const Layout = ({ children, innerPage }) => {
